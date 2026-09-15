1c21dc3ca9a9183e46b143128e55f77f068bb960
javascriptizer <69116290+jwix777ALT@users.noreply.github.com>
2026-09-15 08:44:48 +0300

    Initial

---
+++ backend/src/main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return {"Yo": "peples"}
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
