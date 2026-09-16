4be2da49881aa6e38833403a44fb0fce66135d19
madnessman666 <69116290+madnessman666@users.noreply.github.com>
2026-09-16 17:04:53 +0000

    feat(migrate): add seed command to migration pipeline

--- backend/entrypoint.migrate.sh
+++ backend/entrypoint.migrate.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
+set -e
 alembic upgrade head
+PGPASSWORD=postgres psql -U postgres -h db -p 5432 -d hse_prospective_students -f /app/db_scripts/seed.sql || echo "Seed failed or already applied"
