4fa83626b440265cccd60d922fb065fdf1d014d7
madnessman666 <69116290+madnessman666@users.noreply.github.com>
2026-09-16 17:29:53 +0000

    feat(infra): HOTFIX: attempt to fix an issue with migration

--- backend/entrypoint.migrate.sh
+++ backend/entrypoint.migrate.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 set -e
+
+echo "Waiting for database..."
+while ! pg_isready -h db -p 5432 -U postgres; do
+  sleep 1
+done
+echo "Database is ready!"
+
 alembic upgrade head
 PGPASSWORD=postgres psql -U postgres -h db -p 5432 -d hse_prospective_students -f /app/db_scripts/seed.sql || echo "Seed failed or already applied"
